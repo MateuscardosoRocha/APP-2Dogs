03a9171c988405976e62bdd2b5ea4c1a97507998
Gilson Jr <gindrejunior@hotmail.com>
2023-08-13 09:45:23 -0300

    feat: linear gradient

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,44 +1,48 @@
 import { View, Text, StyleSheet, TouchableOpacity, TextInput, ImageBackground, Image } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
 
 
 export default function Login() {
- 
+
     const navigation = useNavigation();
     const navigateToAbout = () => {
         navigation.navigate('About'); // Nome da tela de destino definido na navegação
-      };
+    };
 
-   
     return (
         <View style={styles.container}>
             <View style={styles.upperView}>
-                <ImageBackground style={styles.image}  source={require('../../../assets/ouro_fino_skate_eletrico_nikon.png')}>
-                    <View style={styles.logoView}>
-                        <Image style={styles.logo} source={require('../../../assets/logo_registrado.png')} />
-                    </View>
-                    <View>
-                    </View>
-                    <View style={styles.inputView}>
-                        <View style={styles.welcomeView}>
-                            <Text style={styles.welcomeText}>Entre para o</Text>
-                            <Text style={styles.welcomeText}>Universo Two Dogs</Text>
+                <ImageBackground style={styles.image} source={require('../../../assets/ouro_fino_skate_eletrico_nikon.png')}>
+                    <LinearGradient style={styles.gradient} colors={['transparent', '#000000']}>
+                        <View style={styles.logoView}>
+                            <Image style={styles.logo} source={require('../../../assets/logo_registrado.png')} />
                         </View>
-                        <TextInput style={styles.input} placeholder='E-mail'></TextInput>
-                        <TextInput style={styles.input} placeholder='Password'></TextInput>
-                        <View style={styles.forgotPasswordView}>
-                            <TouchableOpacity>
-                                <Text style={styles.forgotPassword}>Esqueceu sua senha?</Text>
-                            </TouchableOpacity>
+                        <View>
                         </View>
-                    </View>
+                        <View style={styles.inputView}>
+                            <View style={styles.welcomeView}>
+                                <Text style={styles.welcomeText}>Entre para o</Text>
+                                <Text style={styles.welcomeText}>Universo Two Dogs</Text>
+                            </View>
+                            <TextInput style={styles.input} placeholder='E-mail'></TextInput>
+                            <TextInput style={styles.input} placeholder='Password'></TextInput>
+                            <View style={styles.forgotPasswordView}>
+                                <TouchableOpacity>
+                                    <Text style={styles.forgotPassword}>Esqueceu sua senha?</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </LinearGradient>
                 </ImageBackground>
             </View>
             <View style={styles.lowerView}>
-                <TouchableOpacity style={styles.login} onPress={navigateToAbout} >
-                    <Text>Entrar</Text>
-                </TouchableOpacity>
+                <View style={styles.login}>
+                    <TouchableOpacity style={styles.btnLogin} onPress={navigateToAbout} >
+                        <Text>Entrar</Text>
+                    </TouchableOpacity>
+                </View>
                 <View style={styles.register}>
                     <View style={styles.registerText}>
                         <Text style={styles.registerInfo}>Tem um produto Two Dogs e ainda não se cadastrou?</Text>
@@ -67,6 +71,9 @@ const styles = StyleSheet.create({
         height: '100%',
         width: '100%'
     },
+    gradient: {
+        height: '100%',
+    },
     logoView: {
         marginTop: 70,
         display: 'flex',
@@ -82,7 +89,6 @@ const styles = StyleSheet.create({
         display: 'flex',
         flex: 0.3,
         alignItems: 'center',
-        
     },
     inputView: {
         display: 'flex',
@@ -111,7 +117,6 @@ const styles = StyleSheet.create({
     forgotPasswordView: {
         display: 'flex',
         width: '70%',
-       
         alignItems: 'flex-end',
     },
     forgotPassword: {
@@ -119,6 +124,12 @@ const styles = StyleSheet.create({
         fontWeight: 'bold'
     },
     login: {
+        display: 'flex',
+        alignItems: 'center',
+        backgroundColor: '#ffffff',
+        width: '100%',
+    },
+    btnLogin: {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
@@ -138,20 +149,15 @@ const styles = StyleSheet.create({
         backgroundColor: '#ffffff',
         paddingBottom: '8%',
         width: '100%',
-        
-        
     },
     registerText: {
         width: '50%',
         paddingBottom: 20,
         fontWeight: 'extra-bold'
-
-        
     },
     registerInfo: {
         fontWeight: 'bold',
         textAlign: 'center',
-       
     },
     btnRegister: {
         display: 'flex',
